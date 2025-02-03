60f8a85d9ee2e1b0b12aab4d632550639c06862b
GANESH_777 <95033308+GaneshWiz07@users.noreply.github.com>
2025-02-03 16:12:21 +0530

    Updated

--- FLASK-APP/form1.py
+++ FLASK-APP/form1.py
@@ -10,6 +10,14 @@ def form():
     if form.validate_on_submit():
        return f"Hello, {form.name.data}!"
     return render_template('form.html', form=form)
+@app.errorhandler(404)
+def not_found(error):
+    return "PAGE NOT FOUND", 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return "Internal Server Error", 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
